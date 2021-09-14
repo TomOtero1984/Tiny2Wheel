436eb897ff7e1862a77effdf719f84d2dfdb64e9
Tom <thomasotero@novuslabs.com>
2021-09-14 16:02:26 -0700

    Updating Arduino code and adding atmega328a docs

--- arduino/t2w_arduino_controller.ino
+++ arduino/t2w_arduino_controller.ino
@@ -1,12 +1,11 @@
 #include "Arduino.h"
 
-void setup()
-{
+void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
 }
 
-void loop()
-{
+void loop() {
     digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
     delay(1000);                     // wait for a second
     digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
